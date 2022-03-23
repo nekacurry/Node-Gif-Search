322fb9a13d4b403816fc436dadd19fee2178d18d
nekacurry <anneka498@gmail.com>
2022-03-23 17:45:36 -0500

    added styles

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const { engine } = require('express-handlebars');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
+
 const app = express();
+app.use(express.static('public'));
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set("views", "./views");
